39aa939bd28da1f3e1e00e07647aad3ec0cd370a
JayJay <timothyjapolinario@gmail.com>
2023-01-14 22:29:58 +0800

    Add styling to user name input

--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,24 @@
 import "./User.css";
 const User = ({ submitUser }) => {
   return (
-    <div id="user-name-form">
+    <div id="user-name-form-wrapper">
       <form
+        id="user-name-form"
         onSubmit={(e) => {
           e.preventDefault();
           submitUser(document.getElementById("user-name-input").value);
         }}
       >
-        <label htmlFor="user-name-input">UserName</label>
+        <label htmlFor="user-name-input" id="user-name-input-label">
+          UserName
+        </label>
         <input id="user-name-input" />
         <button
           type="button"
           onClick={() => {
             submitUser(document.getElementById("user-name-input").value);
           }}
+          id="user-submit-button"
         >
           Submit
         </button>
