30c5fc3cc8421e6f5854902b3ff880761b37e613
JayJay <timothyjapolinario@gmail.com>
2023-01-16 18:50:12 +0800

    Added instruction screen

---
+++ src/components/Instruction.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./Instruction.css";
+const Instruction = ({ closeInstruction }) => {
+  return (
+    <div className="Instruction">
+      <div>
+        <div className="section-title">Reminders:</div>
+        <div className="section-content">
+          <ul>
+            <li>If you are playing on mobile, kindly zoom out.</li>
+            <li>Do not play on the Selection Map Menu.</li>
+            <li>Just enjoy the game, not competitively.</li>
+            <li>
+              This game is easy to cheat on, but do not be pathetic to do it.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div className="section-title">Task</div>
+        <div className="section-content">
+          On each map, a list of pokemon will be given out. It is your job to
+          find those pokemons. If you found them on the map, just click them and
+          click their icon on the menu besides the circle where you click them.
+        </div>
+      </div>
+      <div
+        className="button-close-instruction"
+        onClick={() => {
+          console.log("???");
+          closeInstruction();
+        }}
+      >
+        I understand.
+      </div>
+    </div>
+  );
+};
+
+export default Instruction;
--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import Instruction from "./Instruction";
 import LoadingScreen from "./LoadingScreen";
 import MapCard from "./MapCard";
 import "./StartMenu.css";
 import User from "./User";
 const StartMenu = ({ maps, selectMap, isLoading, user, submitUser }) => {
+  const [isInstruction, setInstruction] = useState(true);
   if (isLoading) {
     return <LoadingScreen />;
+  } else if (isInstruction) {
+    return (
+      <Instruction
+        closeInstruction={() => {
+          setInstruction(false);
+        }}
+      />
+    );
   } else {
     if (!user.name) {
       return <User submitUser={submitUser} />;
